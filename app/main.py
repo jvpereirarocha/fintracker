4e89dc2469f74d0f6f2c66ddf2ef67423eda96c9
Joao Victor <jvpr.tech@gmail.com>
2024-07-23 00:09:33 -0300

    updating container structure of the project

--- app/main.py
+++ app/main.py
@@ -1,11 +1,58 @@
-from typing import Annotated
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI
+from http import HTTPStatus
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
 
+from app.database import User
 from app.dependencies import get_db
+from app.domain.entities.users import UserEntity
+from app.schemas.users import CreateUser, UserPublic
+
 
 app = FastAPI(title="Fintracker API")
 
-@app.get("/")
-def read_root(db: Annotated[None, Depends(get_db)]):
-    print(f"DB: {dir(db)}")
-    return {"message": "hello, world!"}
+
+users_router = APIRouter(
+    prefix="/users",
+    dependencies=[Depends(get_db)]
+)
+
+
+@users_router.post(
+    "/",
+    response_model=UserPublic,
+)
+async def create_user(user_schema: CreateUser):
+    breakpoint()
+    db = await get_db()
+    user_already_exists = db.scalar(
+        select(User).where(
+            User.email == user_schema.email
+            |
+            User.username == user_schema.username
+        )
+    )
+    if user_already_exists:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail="User already exists"
+        )
+    user_entity = UserEntity.new_user(
+        user_schema=user_schema
+    )
+    db_user = User(
+        email=user_entity.email,
+        password=user_entity.hashed_password,
+        username=user_entity.username
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh()
+    user_public = UserPublic(
+        username=db_user.username,
+        email=db_user.email
+    )
+    return {"message": "User created!", "user": user_public}
+
+
+app.include_router(router=users_router)
---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+echo "Exporting environment variables..."
+export $(grep -v '^#' .env | xargs)
+
+echo "Now, let's run the migrations"
+alembic upgrade head
+
+echo "Latest, we will run the server"
+uvicorn 'app.main:app' --host 0.0.0.0 --port 8000
