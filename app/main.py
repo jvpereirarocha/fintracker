92c68268e04c5efaaea5afae51bce49b7f266255
Joao Victor <jvpr.tech@gmail.com>
2024-07-25 00:06:51 -0300

    fix: fixing create user endpoint

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
+from datetime import datetime
 from fastapi import FastAPI
 from http import HTTPStatus
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
 
 from app.database import User
-from app.dependencies import get_db
+from app.dependencies import get_session
 from app.domain.entities.users import UserEntity
 from app.schemas.users import CreateUser, UserPublic
 
@@ -14,7 +15,7 @@ app = FastAPI(title="Fintracker API")
 
 users_router = APIRouter(
     prefix="/users",
-    dependencies=[Depends(get_db)]
+    dependencies=[Depends(get_session)]
 )
 
 
@@ -23,13 +24,12 @@ users_router = APIRouter(
     response_model=UserPublic,
 )
 async def create_user(user_schema: CreateUser):
-    breakpoint()
-    db = await get_db()
-    user_already_exists = db.scalar(
+    session = await get_session()
+    user_already_exists = session.scalar(
         select(User).where(
-            User.email == user_schema.email
+            (User.email == user_schema.email)
             |
-            User.username == user_schema.username
+            (User.username == user_schema.username)
         )
     )
     if user_already_exists:
@@ -43,11 +43,12 @@ async def create_user(user_schema: CreateUser):
     db_user = User(
         email=user_entity.email,
         password=user_entity.hashed_password,
-        username=user_entity.username
+        username=user_entity.username,
+        transactions=[],
+        updated_at=datetime.now()
     )
-    db.add(db_user)
-    db.commit()
-    db.refresh()
+    session.add(db_user)
+    session.commit()
     user_public = UserPublic(
         username=db_user.username,
         email=db_user.email
