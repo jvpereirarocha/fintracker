b43e32dcfadee8a104d528a078201c12b408a16b
Joao Victor <jvpereirarocha@gmail.com>
2024-09-07 16:37:20 -0300

    feat: add new dependency in the project

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,6 +1,14 @@
+from fastapi import Request
 from app.database import Session
+from app.middleware import get_user_info
+from app.schemas.auth import JWTPayload
 
 
 async def get_session():
     with Session() as session:
         return session
+
+
+async def get_current_user(request: Request):
+    user_info = await get_user_info(request=request)
+    return user_info
