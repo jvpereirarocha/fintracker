2c5fa47b1651edffbb6c4cc37fae62e99687135b
Joao Victor <jvpereirarocha@gmail.com>
2024-09-07 16:39:50 -0300

    feat: including new route to the app

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from app.routes.users import users_router
+from app.routes.transactions import transactions_router
 
 
 app = FastAPI(title="Fintracker API")
 
 
 app.include_router(router=users_router)
+app.include_router(router=transactions_router)
