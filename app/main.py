ea2d9f702c25eff9bcb1d20a7c9930c53f153d16
Joao Victor <jvpereirarocha@gmail.com>
2024-09-22 10:27:40 -0300

    feat: adding CORS Middleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from app.routes.users import users_router
 from app.routes.transactions import transactions_router
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Fintracker API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 app.include_router(router=users_router)
