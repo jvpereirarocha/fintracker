247edd6126818c5bda6941a4900783bd8aef2544
Joao Victor <jvpereirarocha@gmail.com>
2026-03-12 08:35:02 -0300

    feat: including new api router into the app

--- app/main.py
+++ app/main.py
@@ -2,14 +2,21 @@ from fastapi import FastAPI
 from app.routes.users import users_router
 from app.routes.transactions import transactions_router
 from app.routes.dashboard import dashboard_router
+from app.routes.categories import category_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Fintracker API")
 
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://0.0.0.0:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -18,3 +25,4 @@ app.add_middleware(
 app.include_router(router=users_router)
 app.include_router(router=transactions_router)
 app.include_router(router=dashboard_router)
+app.include_router(router=category_router)
