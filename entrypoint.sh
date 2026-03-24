5300cbccf0d7e0e573970c83acef88a34d5a8726
Joao Victor <jvpereirarocha@gmail.com>
2026-03-24 17:54:53 -0300

    refactor: adapting k8s manifests

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
+#!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
 echo "First of all, let's run the migrations"
 alembic upgrade head
 
 echo "Latest, we will run the server"
-uvicorn 'app.main:app' --host 0.0.0.0 --port 8000 --reload
+exec uvicorn 'app.main:app' --host 0.0.0.0 --port 8000 --reload
