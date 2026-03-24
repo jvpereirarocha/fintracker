5ef1d01feb680d899b5f20f80e91c4be055e71bf
Joao Victor <jvpereirarocha@gmail.com>
2026-03-24 11:10:47 -0300

    chore: removing schemas

--- app/schemas/dashboard.py
+++
@@ -1,8 +0,0 @@
-from pydantic import Field, BaseModel
-
-
-class DashboardResponse(BaseModel):
-    monthly_revenues: str = Field(alias="monthlyRevenues")
-    monthly_expenses: str = Field(alias="monthlyExpenses")
-    yearly_revenues: str = Field(alias="yearlyRevenues")
-    yearly_expenses: str = Field(alias="yearlyExpenses")
